a1c7c3cab833506257b8251aeefa239817cbb969
JacksonBurns <jwburns@mit.edu>
2025-01-17 11:57:09 -0500

    split using kmeans for val

--- main.py
+++ main.py
@@ -24,15 +24,19 @@ data_dir.mkdir(exist_ok=True)
 calc = Calculator(descriptors, ignore_3D=True)
 
 train_file = data_dir / "train.parquet"
+val_file = data_dir / "validation.parquet"
 if not train_file.exists():
     train_data = pd.read_csv(data_dir / "AqSolDBc.csv")
     train_data["rdkit_mol"] = train_data["SmilesCurated"].apply(MolFromSmiles)
     train_data = train_data.dropna(axis=0, subset=["rdkit_mol"])
+    # determine our own training and validation sets
+    *_, train_idxs, val_idxs = train_test_split_molecules(train_data["rdkit_mol"], sampler="kmeans", train_size=0.80, test_size=0.20, return_indices=True)
     train_descs: pd.DataFrame = calc.pandas(train_data["rdkit_mol"]).fill_missing()
     train_df = pd.concat((train_data[["ExperimentalLogS"]], train_descs), axis=1)
-    train_df.to_parquet(train_file)
-else:
-    train_df = pd.read_parquet(train_file)
+    train_df.iloc[train_idxs].to_parquet(train_file)
+    train_df.iloc[val_idxs].to_parquet(val_file)
+train_df = pd.read_parquet(train_file)
+val_df = pd.read_parquet(val_file)
 
 test_file = data_dir / "test.parquet"
 if not test_file.exists():
@@ -42,12 +46,12 @@ if not test_file.exists():
     test_descs: pd.DataFrame = calc.pandas(test_data["rdkit_mol"]).fill_missing()
     test_df = pd.concat((test_data[["LogS"]], test_descs), axis=1)
     test_df.to_parquet(test_file)
-else:
-    test_df = pd.read_parquet(test_file)
-    # match the target column names
-    test_df = test_df.rename(columns={"LogS": "ExperimentalLogS"})
+test_df = pd.read_parquet(test_file)
+# match the target column names
+test_df = test_df.rename(columns={"LogS": "ExperimentalLogS"})
 
 train_data = TabularDataset(train_df)
+val_data = TabularDataset(val_df)
 test_data = TabularDataset(test_df)
 
 # training from scratch
@@ -63,8 +67,11 @@ predictor = TabularPredictor(
 )
 predictor.fit(
     train_data=train_data,
+    tuning_data=val_data,
+    use_bag_holdout=True,
+    test_data=test_data,  # not seen during training
     num_gpus=8,
-    presets='experimental',
+    presets='best_quality',
     time_limit=3600*100,  # 100 hours
 )
 # initial quick run
