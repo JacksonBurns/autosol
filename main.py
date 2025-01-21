4e336997a4db0dd9499db136249c518da0ce8f25
JacksonBurns <jwburns@mit.edu>
2025-01-21 10:58:01 -0500

    kmeans sampling and random resutls

--- main.py
+++ main.py
@@ -17,6 +17,11 @@ from autogluon.tabular import TabularDataset, TabularPredictor
 from mordred import Calculator, descriptors
 import pandas as pd
 from rdkit.Chem import MolFromSmiles
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from matplotlib.axes import Axes
+from sklearn.metrics import mean_squared_error, r2_score
 
 data_dir = Path("data")
 data_dir.mkdir(exist_ok=True)
@@ -55,7 +60,7 @@ val_data = TabularDataset(val_df)
 test_data = TabularDataset(test_df)
 
 # training from scratch
-outdir = '/datai/autogluon_extrapolation'
+outdir = '/datai/autogluon_random_winsorization'
 if Path(outdir).exists():
     print("Output dir already exists, exiting to avoid overwrite!")
     exit(1)
@@ -76,6 +81,65 @@ predictor.fit(
 )
 # initial quick run
 # predictor = TabularPredictor.load("/home/jwburns/autosol/AutogluonModels/ag-20250113_212151")
+# predictor = TabularPredictor.load("/datai/autogluon")  # best_quality random splitting based model
 
-print(f"Test Set Performance:", predictor.evaluate(test_data))
-print(predictor.leaderboard(test_data))
+# print(f"Test Set Performance:", predictor.evaluate(test_data))
+predictions = predictor.predict(test_data, as_pandas=False)
+
+data = pd.DataFrame(
+    {
+        "truth": test_data["ExperimentalLogS"],
+        "prediction": predictions,
+    }
+)
+
+# Create the plot
+fig, ax = plt.subplots(figsize=(5, 4), constrained_layout=True)
+ax: Axes  # type hints, please
+ax.grid(True, which="major", axis="both")
+ax.set_axisbelow(True)
+hb = ax.hexbin(x=data["truth"], y=data["prediction"], gridsize=70, cmap="viridis", mincnt=1)
+
+cb = fig.colorbar(hb, ax=ax)
+cb.set_label("Number of compounds")
+ax.plot([-12, 2], [-12, 2], "r", linewidth=1)
+ax.plot([-12, 2], [-11, 3], "r--", linewidth=0.5)
+ax.plot([-12, 2], [-13, 1], "r--", linewidth=0.5)
+ax.set_title("autosol")
+ax.set_xlabel("Solubility (LogS)")
+ax.set_ylabel("cLogS")
+ax.set_xlim(-12, 2)
+ax.set_ylim(-12, 2)
+
+# Text box for R2 and MSE
+textstr = "\n".join(
+    (
+        f"$\\bf{{R2}}:$ {r2_score(data['truth'], data['prediction']):.2f}",
+        f"$\\bf{{MSE}}:$ {mean_squared_error(data['truth'], data['prediction']):.2f}",
+    )
+)
+ax.text(
+    -8.55,
+    -2.1,
+    textstr,
+    transform=ax.transData,
+    fontsize=10,
+    verticalalignment="top",
+    horizontalalignment="right",
+)
+
+# Pie chart
+_frac_wn_1 = np.count_nonzero(np.abs(data["truth"] - data["prediction"]) < 1.0) / len(data)
+sizes = [1 - _frac_wn_1, _frac_wn_1]
+ax_inset = ax.inset_axes([-12, -2, 4, 4], transform=ax.transData)
+ax_inset.pie(
+    sizes,
+    colors=["#ae2b27", "#4073b2"],
+    startangle=360 * (_frac_wn_1 - 0.5) / 2,
+    wedgeprops={"edgecolor": "black"},
+    autopct="%1.f%%",
+    textprops=dict(color="w"),
+)
+ax_inset.axis("equal")
+
+plt.savefig("parity.png")
